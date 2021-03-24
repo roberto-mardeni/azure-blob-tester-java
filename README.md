d836a102a6a5fbe5edb416a8e174a1d571645b59
roberto-mardeni <rmardeni@gmail.com>
2021-03-24 17:24:08 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # azure-blob-tester-java
 A sample demonstrating how to upload/download blobs to an Azure Storage Account using the Azure Java SDK v12
+
+# usage
+By default the application will use the [Azure Storage Emulator](https://docs.microsoft.com/en-us/azure/storage/common/storage-use-emulator) connection string. 
+
+If you want to test against an Azure Storage Account, provide an environment variable **AZURE_STORAGE_CONNECTION_STRING** with the connection string like below.
+
+```bash
+setx AZURE_STORAGE_CONNECTION_STRING "<Azure Storage Account Connection String>"
+
+-- or --
+
+$env:AZURE_STORAGE_CONNECTION_STRING = "<Azure Storage Account Connection String>"
+```
+
+To include random sleep commands, include a **ENABLE_RANDOM_SLEEPS** environment variable with the value **true** like below.
+
+```bash
+setx ENABLE_RANDOM_SLEEPS "true/false"
+
+-- or --
+
+$env:ENABLE_RANDOM_SLEEPS = "true/false"
+```
+
+# packaging
+The project file has been customized to package with and without all dependencies for convenience.
---
+++ src/main/java/com/blobs/tester/App.java
@@ -0,0 +1,109 @@
+package com.blobs.tester;
+
+/**
+ * Azure blob storage v12 SDK quickstart
+ */
+import com.azure.core.http.policy.*;
+import com.azure.storage.blob.*;
+import com.azure.storage.blob.models.*;
+// import com.azure.storage.common.policy.RequestRetryOptions;
+
+import java.io.*;
+
+public class App {
+    static Boolean randomSleepsEnabled = false;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Azure Blob Storage v12 - Java quickstart sample\n");
+
+        String randomSleepsEnabledSetting = System.getenv("ENABLE_RANDOM_SLEEPS");
+
+        randomSleepsEnabled = (randomSleepsEnabledSetting != null
+                && randomSleepsEnabledSetting.toLowerCase().equalsIgnoreCase("true"));
+
+        // Retrieve the connection string for use with the application. The storage
+        // connection string is stored in an environment variable on the machine
+        // running the application called AZURE_STORAGE_CONNECTION_STRING. If the
+        // environment variable
+        // is created after the application is launched in a console or with
+        // Visual Studio, the shell or application needs to be closed and reloaded
+        // to take the environment variable into account.
+        String connectStr = System.getenv("AZURE_STORAGE_CONNECTION_STRING");
+
+        if (connectStr == null) {
+            connectStr = "UseDevelopmentStorage=true";
+        } else {
+            System.out.println("Using connection string from environment");
+        }
+
+        // Create a BlobServiceClient object which will be used to create a container
+        // client
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectStr)
+                // .retryOptions(new RequestRetryOptions())
+                // .addPolicy(new RetryPolicy())
+                .httpLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BASIC)).buildClient();
+
+        // Create a unique name for the container
+        String containerName = "quickstartblobs" + java.util.UUID.randomUUID();
+
+        // Create the container and return a container client object
+        BlobContainerClient containerClient = blobServiceClient.createBlobContainer(containerName);
+
+        System.out.println(String.format("\nCreated container %s", containerName));
+
+        // Upload local files
+        // Thread.currentThread().getContextClassLoader().getResource("");
+        String localPath = "src/main/resources";
+        File folder = new File(localPath);
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                String blobName = listOfFiles[i].getName();
+                System.out.println("\tUploading " + blobName);
+                BlobClient blobClient = containerClient.getBlobClient(blobName);
+                blobClient.uploadFromFile(listOfFiles[i].getAbsolutePath());
+
+                randomSleep();
+            }
+        }
+
+        // List and download the blob(s) in the container.
+        System.out.println(String.format("\n\nListing and downloading blobs... in %s", containerName));
+
+        String localDownloadPath = System.getProperty("java.io.tmpdir");
+
+        for (BlobItem blobItem : containerClient.listBlobs()) {
+            String blobName = blobItem.getName();
+            System.out.println("\tDownloading " + blobName);
+            BlobClient blobClient = containerClient.getBlobClient(blobName);
+            blobClient.downloadToFile(localDownloadPath + blobName, true);
+
+            randomSleep();
+        }
+
+        // Clean up
+        System.out.println("\nPress the Enter key to begin clean up");
+        System.console().readLine();
+
+        System.out.println("Deleting blob container...");
+        containerClient.delete();
+
+        System.out.println("Done");
+    }
+
+    private static void randomSleep() {
+        if (randomSleepsEnabled) {
+            // thread to sleep randomly between 100 and 500 milliseconds
+            try {
+                int min = 100;
+                int max = 300;
+                long sleepTime = (long) (Math.random() * (max - min + 1) + min);
+                System.out.println("\tSleeping for " + sleepTime);
+                Thread.sleep(sleepTime);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
